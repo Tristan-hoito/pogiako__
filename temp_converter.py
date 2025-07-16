39b764ca844205cd873672d995f4b97e56e94163
Tristan-hoito <tristanragasa17@gmail.com>
2025-07-16 13:43:32 +0800

    Update temp_converter.py

--- temp_converter.py
+++ temp_converter.py
@@ -1,14 +1,13 @@
 temp_value = float(input("Enter Temperature Number:"))
-unit = input('Enter Temperature Unit F/C:')
+unit = input('Enter Temperature Unit F/C:').upper()
 
 if unit == 'F':
     result = ((9 / 5) * temp_value) + 32
-    funit = 'C'
+    funit = '°C'
 elif unit == 'C':
     result = (temp_value - 32) * 5 / 9
-    funit = 'F'
+    funit = '°F'
 else:
     result = 'Invalid'
     funit = 'Results'
-
-print(result, funit)
+print(f'Result: {result:.2f} {funit}')
